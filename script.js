ce6d04f2ec28081ef8f9626dcd621ead9821aa30
Sonam Thakkar <sonamthakkar@sonams-mbp.lan>
2022-10-05 16:14:53 -0700

    updated version

---
+++ script.js
@@ -0,0 +1,10 @@
+let mainEl = document.querySelector("main")
+console.log(mainEl)
+
+
+mainEl.style.backgroundColor = 
+
+mainEl.innerHTML = '<h1>Quote</h1>'
+
+mainEl.classList.add('.menucontents')
+
